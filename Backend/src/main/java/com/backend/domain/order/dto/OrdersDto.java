72e766d564c218f3f5e602c4022ba0486b3de8f1
hznnoy <yeonii9902@gmail.com>
2025-09-25 11:42:47 +0900

    feat: 주문 상태를 OrderStatus enum으로 변경

--- Backend/src/main/java/com/backend/domain/order/dto/OrdersDto.java
+++ Backend/src/main/java/com/backend/domain/order/dto/OrdersDto.java
@@ -1,5 +1,6 @@
 package com.backend.domain.order.dto;
 
+import com.backend.domain.order.entity.OrderStatus;
 import com.backend.domain.order.entity.Orders;
 
 import java.time.LocalDateTime;
@@ -11,7 +12,7 @@ public record OrdersDto (
         int zipCode,
         int totalPrice,
         LocalDateTime orderDate,
-        String status,
+        OrderStatus status,
         boolean canModify // 14시 기준 수정 가능 여부
 ){
     public OrdersDto(Orders orders, boolean canModify){
