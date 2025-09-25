0aeb977f1833b29297328722652a98b5bf8eb122
hznnoy <yeonii9902@gmail.com>
2025-09-25 12:40:52 +0900

    feat: 주문 목록 조회, 주문 상세 조회 구현

--- Backend/src/main/java/com/backend/domain/order/repository/OrdersRepository.java
+++ Backend/src/main/java/com/backend/domain/order/repository/OrdersRepository.java
@@ -2,8 +2,14 @@ package com.backend.domain.order.repository;
 
 import com.backend.domain.order.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrdersRepository extends JpaRepository<Orders, Integer> {
+    @Query("SELECT o FROM Orders o WHERE o.email = :email ORDER BY o.orderDate DESC")
+    List<Orders> findByEmailOrderByOrderDateDesc(@Param("email") String email);
 }
