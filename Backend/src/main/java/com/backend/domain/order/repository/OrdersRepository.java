eebbdce84444b0e13de404671ebad0eed3b14a1e
hznnoy <yeonii9902@gmail.com>
2025-09-26 11:07:26 +0900

    feat: 14시 기준 자동 주문 상태 변경(PENDING -> CONFIRMED)

--- Backend/src/main/java/com/backend/domain/order/repository/OrdersRepository.java
+++ Backend/src/main/java/com/backend/domain/order/repository/OrdersRepository.java
@@ -1,15 +1,15 @@
 package com.backend.domain.order.repository;
 
+import com.backend.domain.order.entity.OrderStatus;
 import com.backend.domain.order.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface OrdersRepository extends JpaRepository<Orders, Integer> {
-    @Query("SELECT o FROM Orders o WHERE o.email = :email ORDER BY o.orderDate DESC")
-    List<Orders> findByEmailOrderByOrderDateDesc(@Param("email") String email);
+    List<Orders> findByEmailOrderByOrderDateDesc(String email);
+
+    List<Orders> findByStatus(OrderStatus orderStatus);
 }
