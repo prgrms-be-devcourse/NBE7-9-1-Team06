fec0fd4931ee1c70e9486167e80ace8da3350488
hznnoy <yeonii9902@gmail.com>
2025-09-29 11:01:04 +0900

    feat: RsData 추가

--- Backend/src/main/java/com/backend/global/rsData/RsData.java
+++ Backend/src/main/java/com/backend/global/rsData/RsData.java
@@ -1,5 +1,6 @@
 package com.backend.global.rsData;
 
+<<<<<<< HEAD
 import lombok.Getter;
 
 @Getter
@@ -17,4 +18,30 @@ public class RsData<T> {
         this.msg = msg;
         this.data = data;
     }
+=======
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class RsData<T> {
+
+    private String resultCode;
+    private String msg;
+    private T data;
+
+    public RsData(String resultCode, String msg) {
+        this.resultCode = resultCode;
+        this.msg = msg;
+        this.data = null;
+    }
+
+    @JsonIgnore
+    public int getStatusCode() {
+        String statusCode = resultCode.split("-")[0];
+        return Integer.parseInt(statusCode);
+    }
+
+>>>>>>> 3b619eb (feat : 주문 취소 구현, RsData 생성)
 }
